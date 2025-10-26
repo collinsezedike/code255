178dc467b10f9727dc04fb7307bc13e764cfef02
Collins Ezedike-egwom <ezedikecollins@gmail.com>
2025-10-27 00:54:24 +0100

    test: complete create game test

--- anchor/code255/tests/code255.ts
+++ anchor/code255/tests/code255.ts
@@ -1,16 +1,47 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { Code255 } from "../target/types/code255";
+import { expect } from "chai";
 
 describe("code255", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+	anchor.setProvider(anchor.AnchorProvider.env());
 
-  const program = anchor.workspace.code255 as Program<Code255>;
+	const program = anchor.workspace.code255 as Program<Code255>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
-  });
+	const SYSTEM_PROGRAM_ID = anchor.web3.SystemProgram.programId;
+
+	let admin: anchor.web3.Keypair;
+	let game: anchor.web3.PublicKey;
+
+	const gameCode = new anchor.BN(Math.floor(Math.random() * 9e7) + 1e7);
+
+	before(async () => {
+		admin = anchor.getProvider().wallet.payer;
+
+		[game] = anchor.web3.PublicKey.findProgramAddressSync(
+			[Buffer.from("game"), gameCode.toBuffer("le", 8)],
+			program.programId
+		);
+	});
+
+	it("Create game", async () => {
+		await program.methods
+			.createGame(gameCode)
+			.accountsStrict({
+				admin: admin.publicKey,
+				game,
+				systemProgram: SYSTEM_PROGRAM_ID,
+			})
+			.signers([admin])
+			.rpc();
+
+		const gameAccount = await program.account.game.fetch(game);
+		expect(gameAccount.admin.toBase58()).equals(admin.publicKey.toBase58());
+		expect(gameAccount.gameCode.toNumber()).to.equal(gameCode.toNumber());
+		expect(gameAccount.activePlayers).to.equal(0);
+		expect(gameAccount.submittedActions).to.equal(0);
+		expect(gameAccount.round).to.equal(0);
+		expect(gameAccount.roundSeed).to.be.null;
+		expect(gameAccount.playersHash).to.be.null;
+	});
 });
